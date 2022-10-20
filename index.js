0e4f4a87546065c79c3664f24c8ff6e1e96f6843
misaelsarm <misaelsarmiento97@gmail.com>
2022-10-20 10:15:08 -0500

    removed logs

--- index.js
+++ index.js
@@ -6,12 +6,11 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-  console.log(req)
   res.send('Hello world')
 })
 
 app.post('/webhook', (req, res) => {
-  console.log(req.body)
+  console.log({ body: req.body })
 })
 
 console.log(process.env.PORT)
