895a64da0b62716429bb449367cb3d0a377fae10
misaelsarm <misaelsarmiento97@gmail.com>
2022-10-20 10:06:45 -0500

    fixed port

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express')
+require('dotenv').config()
 
 const app = express()
 
+const port = process.env.PORT || 3000
+
 app.get('/', (req, res) => {
+  console.log(req)
   res.send('Hello world')
 })
 
@@ -10,4 +14,8 @@ app.post('/webhook', (req, res) => {
   console.log(req.body)
 })
 
-app.listen(8080)
+console.log(process.env.PORT)
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
